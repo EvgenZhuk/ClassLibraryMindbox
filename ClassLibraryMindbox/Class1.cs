8358bdd962b6477d432d99703b20cd3aaa0eefd7
Евгений <deus_man@mail.ru>
2021-02-22 21:16:20 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryMindbox/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibraryMindbox
+{
+    public class Class1
+    {
+        public double AreaOfCircle (double R)
+        {
+            double S = Math.PI * Math.Pow(R, 2); 
+            return S;
+        }
+
+        public double AreaOfTriangle(double L1, double L2, double L3)
+        {
+            double P = (L1 + L2 + L3) / 2;
+            double S = Math.Sqrt(P * (P - L1) * (P - L2) * (P - L3));
+            return S;
+        }
+
+    }
+}
